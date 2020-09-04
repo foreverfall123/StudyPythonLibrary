2ebfe007a47cdefbd147fe00a8c00a7fe632895b
foreverfall123 <foreverfall@naver.com>
2020-09-05 01:58:45 +0900

    fourth

---
+++ center.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QMainWindow
+from PyQt5.QtCore import QDate, Qt
+
+class MyApp(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.date = QDate.currentDate()
+        self.initUI()
+
+    def initUI(self):
+        self.statusBar().showMessage(self.date.toString(Qt.DefaultLocaleLongDate))
+
+        self.setWindowTitle('Centering')
+        self.resize(500,350)
+        self.center()
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
